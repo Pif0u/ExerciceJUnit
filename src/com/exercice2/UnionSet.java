54f8737333eadb7a1f93073c9b902854b46a0b6b
Pifou <48999464+Pif0u@users.noreply.github.com>
2020-12-09 09:47:48 +0100

    Ajout spécification

--- src/com/exercice2/UnionSet.java
+++ src/com/exercice2/UnionSet.java
@@ -6,11 +6,24 @@ public class UnionSet {
     private Vector v1;
     private Vector v2;
 
+    /**
+     *
+     * Cette fonction fusionne deux vecteur en un nouveau vecteur
+     * Le nouveau vecteur est généré dans l'ordre des paramètres v1 puis v2
+     *
+     * @param v1 Un vector non vide
+     * @param v2 Un vector non vide
+     * @return un nouveau Vector
+     */
     public static Vector<Object> unionSet(Vector<Object> v1, Vector<Object> v2) {
-        Vector<Object> result = new Vector<Object>();
-        result.addAll(v1);
-        result.addAll(v2);
-        return result;
-    }
 
+        try {
+            Vector<Object> result = new Vector<Object>();
+            result.addAll(v1);
+            result.addAll(v2);
+            return result;
+        } catch (NullPointerException e) {
+            return new Vector<>();
+        }
+    }
 }
